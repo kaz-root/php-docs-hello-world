76e119b83c1d53c6883c090aae515f668bf73fcf
kaz-root <92615417+kaz-root@users.noreply.github.com>
2023-05-12 16:48:20 +0900

    db connect

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>接続検証</title>
+    <title>データベース接続検証</title>
 </head>
 <body>
- <?php
-echo "Hello World!";
+<?php
+// MySQLサーバーの接続情報
+$host = "testdb01-murayama.mysql.database.azure.com";
+$username = "mysqladmin";
+$password = "1qaZXsw23edCVfr4";
+$dbname = "test_database";
+// MySQLサーバーへの接続
+    $mysqli = new mysqli($host, $username, $password, $dbname);
+// 接続エラーの確認
+if ($mysqli->connect_error) {
+    http_response_code(500);
+    echo "NG";
+} else {
+    echo "OK";
+}
+    // MySQL接続の終了
+    $mysqli->close();
     ?>
 </body>
 </html>
