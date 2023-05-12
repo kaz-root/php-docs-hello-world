b4da23e28eebce24a0d50434ffce35e144559902
kaz-root <92615417+kaz-root@users.noreply.github.com>
2023-05-12 18:37:47 +0900

    new

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 $conn = mysqli_init();
 
 // Establish the connection
-mysqli_real_connect($conn, 'testdb01-murayama.mysql.database.azure.com', 'mysqladmin', 'qaZXsw23edCVfr4', 'test_database', 3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'testdb01-murayama.mysql.database.azure.com', 'mysqladmin', '1qaZXsw23edCVfr4', 'test_database', 3306, NULL, MYSQLI_CLIENT_SSL);
 
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-} else {
-      echo "<h2>データベース接続OK</h2>";
+    // 接続結果の出力
+    if ($con->connect_error) {
+        echo "<h2>データベース接続NG</h2>";
+    } else {
+        echo "<h2>データベース接続OK</h2>";
     }
+
 
     // MySQL接続の終了
     $conn->close();
