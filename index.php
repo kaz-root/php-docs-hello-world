e55122a7e478442e7eb8ea75899f71ef2cb0a9cf
kaz-root <92615417+kaz-root@users.noreply.github.com>
2023-05-12 17:11:28 +0900

    new

--- index.php
+++ index.php
@@ -4,24 +4,25 @@
     <title>DB接続検証</title>
 </head>
 <body>
-<?php
+    <?php
+    echo "Hello Azure!";
 
-echo "Hello World!";
+    // MySQLサーバーの接続情報
+    $host = "testdb01-murayama.mysql.database.azure.com";
+    $username = "mysqladmin";
+    $password = "1qaZXsw23edCVfr4";
+    $dbname = "test_database";
 
-// MySQLサーバーの接続情報
-$host = "testdb01-murayama.mysql.database.azure.com";
-$username = "mysqladmin";
-$password = "1qaZXsw23edCVfr4";
-$dbname = "test_database";
-// MySQLサーバーへの接続
-$mysqli = new mysqli($host, $username, $password, $dbname);
-// 接続エラーの確認
-if ($mysqli->connect_error) {
-    http_response_code(500);
-    echo "NG";
-} else {
-    echo "OK";
-}
+    // MySQLサーバーへの接続
+    $mysqli = new mysqli($host, $username, $password, $dbname);
+
+    // 接続結果の出力
+    if ($mysqli->connect_error) {
+        echo "<h2>データベース接続NG</h2>";
+    } else {
+        echo "<h2>データベース接続OK</h2>";
+    }
+
     // MySQL接続の終了
     $mysqli->close();
     ?>
