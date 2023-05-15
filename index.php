eeee2fe40453f7e0a89a6a53ec4e709181a3cf2b
kaz-root <92615417+kaz-root@users.noreply.github.com>
2023-05-15 10:44:04 +0900

    new

--- index.php
+++ index.php
@@ -15,12 +15,10 @@ mysqli_real_connect($conn, 'testdb01-murayama.mysql.database.azure.com', 'mysqla
     // 接続結果の出力
     if ($conn->connect_error) {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
-        echo "<h2>データベース接続NG</h2>";
-    } else {
-        echo "<h2>'test_database'データベース接続OK</h2>";
-    }
+    } 
 
-
+    echo "<h2>データベース接続OK</h2>";
+    
     // MySQL接続の終了
     $conn->close();
 ?>
